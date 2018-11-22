8dde7d0957c5b6bd279297e7e6593ce4dc206833
Ravi Shanker Tiwari <ravi.rs.tiwari@gmail.com>
2018-11-23 04:30:42 +0530

    Final Changes

---
+++ ProjectMorgan/ExcelUploader.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ExcelUploader
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ImportBtn = New System.Windows.Forms.Button()
+        Me.BrowseBtn = New System.Windows.Forms.Button()
+        Me.ViewDataBtn = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.filepathLbl = New System.Windows.Forms.Label()
+        Me.DatabaseDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DatabaseDataSet = New ProjectMorgan.DatabaseDataSet()
+        Me.DataBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DataTableAdapter = New ProjectMorgan.DatabaseDataSetTableAdapters.dataTableAdapter()
+        Me.SrNoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AgeDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ISINsDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DatabaseDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DatabaseDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ImportBtn
+        '
+        Me.ImportBtn.Location = New System.Drawing.Point(260, 12)
+        Me.ImportBtn.Name = "ImportBtn"
+        Me.ImportBtn.Size = New System.Drawing.Size(75, 23)
+        Me.ImportBtn.TabIndex = 0
+        Me.ImportBtn.Text = "Import"
+        Me.ImportBtn.UseVisualStyleBackColor = True
+        '
+        'BrowseBtn
+        '
+        Me.BrowseBtn.Location = New System.Drawing.Point(49, 12)
+        Me.BrowseBtn.Name = "BrowseBtn"
+        Me.BrowseBtn.Size = New System.Drawing.Size(75, 23)
+        Me.BrowseBtn.TabIndex = 1
+        Me.BrowseBtn.Text = "Browse"
+        Me.BrowseBtn.UseVisualStyleBackColor = True
+        '
+        'ViewDataBtn
+        '
+        Me.ViewDataBtn.Location = New System.Drawing.Point(443, 12)
+        Me.ViewDataBtn.Name = "ViewDataBtn"
+        Me.ViewDataBtn.Size = New System.Drawing.Size(75, 23)
+        Me.ViewDataBtn.TabIndex = 2
+        Me.ViewDataBtn.Text = "Refresh"
+        Me.ViewDataBtn.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(25, 52)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(144, 355)
+        Me.ListBox1.TabIndex = 3
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.SrNoDataGridViewTextBoxColumn, Me.IdDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.AgeDataGridViewTextBoxColumn, Me.ISINsDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.DataBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(186, 52)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(826, 356)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'filepathLbl
+        '
+        Me.filepathLbl.AutoSize = True
+        Me.filepathLbl.Location = New System.Drawing.Point(0, 0)
+        Me.filepathLbl.Name = "filepathLbl"
+        Me.filepathLbl.Size = New System.Drawing.Size(55, 13)
+        Me.filepathLbl.TabIndex = 5
+        Me.filepathLbl.Text = "filepathLbl"
+        Me.filepathLbl.Visible = False
+        '
+        'DatabaseDataSetBindingSource
+        '
+        Me.DatabaseDataSetBindingSource.DataSource = Me.DatabaseDataSet
+        Me.DatabaseDataSetBindingSource.Position = 0
+        '
+        'DatabaseDataSet
+        '
+        Me.DatabaseDataSet.DataSetName = "DatabaseDataSet"
+        Me.DatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'DataBindingSource
+        '
+        Me.DataBindingSource.DataMember = "data"
+        Me.DataBindingSource.DataSource = Me.DatabaseDataSetBindingSource
+        '
+        'DataTableAdapter
+        '
+        Me.DataTableAdapter.ClearBeforeFill = True
+        '
+        'SrNoDataGridViewTextBoxColumn
+        '
+        Me.SrNoDataGridViewTextBoxColumn.DataPropertyName = "SrNo"
+        Me.SrNoDataGridViewTextBoxColumn.HeaderText = "SrNo"
+        Me.SrNoDataGridViewTextBoxColumn.Name = "SrNoDataGridViewTextBoxColumn"
+        '
+        'IdDataGridViewTextBoxColumn
+        '
+        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "Id"
+        Me.IdDataGridViewTextBoxColumn.HeaderText = "Id"
+        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        '
+        'AgeDataGridViewTextBoxColumn
+        '
+        Me.AgeDataGridViewTextBoxColumn.DataPropertyName = "Age"
+        Me.AgeDataGridViewTextBoxColumn.HeaderText = "Age"
+        Me.AgeDataGridViewTextBoxColumn.Name = "AgeDataGridViewTextBoxColumn"
+        '
+        'ISINsDataGridViewTextBoxColumn
+        '
+        Me.ISINsDataGridViewTextBoxColumn.DataPropertyName = "ISINs"
+        Me.ISINsDataGridViewTextBoxColumn.HeaderText = "ISINs"
+        Me.ISINsDataGridViewTextBoxColumn.Name = "ISINsDataGridViewTextBoxColumn"
+        '
+        'ExcelUploader
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1024, 420)
+        Me.Controls.Add(Me.filepathLbl)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ViewDataBtn)
+        Me.Controls.Add(Me.BrowseBtn)
+        Me.Controls.Add(Me.ImportBtn)
+        Me.Name = "ExcelUploader"
+        Me.Text = "ExcelUploader"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DatabaseDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DatabaseDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ImportBtn As Button
+    Friend WithEvents BrowseBtn As Button
+    Friend WithEvents ViewDataBtn As Button
+    Friend WithEvents excellist As ListBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DatabaseDataSetBindingSource As BindingSource
+    Friend WithEvents DatabaseDataSet As DatabaseDataSet
+    Friend WithEvents filepathLbl As Label
+    Friend WithEvents DataBindingSource As BindingSource
+    Friend WithEvents DataTableAdapter As DatabaseDataSetTableAdapters.dataTableAdapter
+    Friend WithEvents SrNoDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents IdDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents AgeDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents ISINsDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+End Class
---
+++ ProjectMorgan/ExcelUploader.vb
@@ -0,0 +1,90 @@
+﻿
+Imports System.Data.OleDb
+Imports Microsoft.Office.Interop
+
+Public Class ExcelUploader
+
+    Private Sub BrowseBtn_Click(sender As Object, e As EventArgs) Handles BrowseBtn.Click
+        Dim folderDlg As New FolderBrowserDialog
+        folderDlg.Description = "Select the folder which contains data excel files..."
+        folderDlg.ShowNewFolderButton = True
+        If (folderDlg.ShowDialog() = DialogResult.OK) Then
+            ListBox1.Text = folderDlg.SelectedPath
+            filepathLbl.Text = folderDlg.SelectedPath
+            Dim di As New IO.DirectoryInfo(folderDlg.SelectedPath)
+            Dim myFileInfoArr As IO.FileInfo() = di.GetFiles()
+            ListBox1.DataSource = myFileInfoArr
+        End If
+    End Sub
+
+    Private Sub ImportBtn_Click(sender As Object, e As EventArgs) Handles ImportBtn.Click
+        If filepathLbl.Text.Equals("filepathLbl") Then
+            Return
+        End If
+        Dim Fldr() As String = IO.Directory.GetFiles(filepathLbl.Text & "\")
+        Dim file As String
+        Dim accessConn As OleDb.OleDbConnection = New OleDb.OleDbConnection With {
+            .ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\keshav\Desktop\database\Database.mdb"
+        }
+        Try
+            For Each file In Fldr
+                accessConn.Open()
+                Dim excelApp As New Excel.Application
+                Dim excelWorkbooks = excelApp.Workbooks.Open(file)
+                For Each xlWorkSheet In excelApp.Sheets
+                    Try
+                        Dim sheet As Excel.Worksheet = excelApp.Sheets(xlWorkSheet.Name)
+                        Dim rowCount As Integer = sheet.UsedRange.Rows.Count
+                        Dim columnCount As Integer = sheet.UsedRange.Columns.Count
+                        Dim value_range As Excel.Range = sheet.UsedRange()
+                        Dim array As Object = value_range.Value2
+                        If array Is Nothing Then
+                            Console.Out.WriteLine("Nothing in sheet " + xlWorkSheet.Name + " FileName: " + file)
+                        Else
+                            Dim headers As String = ""
+                            Dim rowValues(rowCount, columnCount + 1) As String
+                            Dim insertQuery As String = "insert into data("
+                            For row = 1 To rowCount
+                                Dim insertValues As String = " values("
+                                For column = 1 To columnCount
+                                    Dim seperator = ","
+                                    If columnCount.Equals(column) Then
+                                        seperator = ")"
+                                    End If
+                                    If row.Equals(1) Then
+                                        headers += "[" + array(row, column) + "]" + seperator
+                                    Else
+                                        If array(row, column) IsNot Nothing Then
+                                            insertValues += "'" + array(row, column).ToString + "'" + seperator
+                                        End If
+                                    End If
+                                Next
+                                If insertValues.Equals(" values(") Then
+                                    Continue For
+                                End If
+
+                                Dim cmdStr As String = insertQuery + headers + insertValues
+                                Using comd As New OleDb.OleDbCommand(cmdStr, accessConn)
+                                    comd.ExecuteNonQuery()
+                                End Using
+                            Next
+                        End If
+                    Catch ex As Exception
+                        MessageBox.Show(ex.Message + xlWorkSheet.Name + file)
+                        Console.Out.WriteLine(ex.StackTrace)
+                    End Try
+                Next
+                accessConn.Close()
+                excelApp.Workbooks.Close()
+                excelApp = Nothing
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Console.Out.WriteLine(ex.StackTrace)
+        End Try
+    End Sub
+
+    Private Sub ViewDataBtn_Click(sender As Object, e As EventArgs) Handles ViewDataBtn.Click
+        Me.DataTableAdapter.Fill(Me.DatabaseDataSet.data)
+    End Sub
+End Class
